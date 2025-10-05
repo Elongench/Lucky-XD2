d0f806d43b5fdf16f368c89239eb8f9f4af98d39
Elongench <greatdavid727@gmail.com>
2025-10-05 08:12:29 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
+  SESSION_ID: "malvin~VrcUUBAZ#ZKU956sLOwLly
+n7FlqwOMFR39xYjAMH_owzT9AULJ81",  // add your Session Id here
+  OWNER_NUMBER: "2349015345530", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
 };
